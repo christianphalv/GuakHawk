4beaea9ce02b69f29300e7c1233e07757598d8ca
Anushka Angamuthu <anushkaangamuthu@Anushkas-MacBook-Air.local>
2022-04-02 18:31:50 -0500

    adding favorites and preferences pages

---
+++ favorites/app/src/main/java/com/example/favorites/AmoreFavorited.java
@@ -0,0 +1,14 @@
+package com.example.favorites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AmoreFavorited extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorites_amore);
+    }
+}
