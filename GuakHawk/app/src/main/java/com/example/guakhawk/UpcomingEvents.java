3d752666a7c44c9d345a2a5a036c2716997fce3b
Calvin H <hoff1352@umn.edu>
2022-04-05 15:37:29 -0500

    Added Nav Bar to Calaneard

--- GuakHawk/app/src/main/java/com/example/guakhawk/UpcomingEvents.java
+++ GuakHawk/app/src/main/java/com/example/guakhawk/UpcomingEvents.java
@@ -7,9 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.example.guakhawk.ui.createGroup.CreateGroupFragment;
+
 public class UpcomingEvents extends AppCompatActivity {
 
     ImageButton to_account;
+
+    ImageButton to_friends;
+    ImageButton to_home;
+    ImageButton to_create_group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +33,33 @@ public class UpcomingEvents extends AppCompatActivity {
                     }
                 }
         );
+
+        to_friends = findViewById(R.id.button_friends_page);
+        to_friends.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(UpcomingEvents.this, Friends.class);
+                startActivity(i);
+            }
+        });
+
+        to_home = findViewById(R.id.button_home_page);
+        to_home.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(UpcomingEvents.this, HomePage.class);
+                startActivity(i);
+            }
+        });
+
+        to_create_group = findViewById(R.id.button_new_group);
+        to_create_group.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(UpcomingEvents.this, CreateGroupFragment.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
